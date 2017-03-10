ee359b41d6002fcb5dca94e9d5fa0e1f3c7da2e9
Advait Junnarkar <advaitju@users.noreply.github.com>
2017-03-10 15:30:43 +1100

    Mouse over fade effect for Portfolio gallery images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
-	
+
+	$('.block__content__gallery-image').on('mouseenter', function() {
+		$(this).find('.block__content__gallery-image__description').fadeIn(250)
+	});
+	$('.block__content__gallery-image__description').on('mouseleave', function() {
+		$(this).fadeOut(250)
+	});
+
 });
