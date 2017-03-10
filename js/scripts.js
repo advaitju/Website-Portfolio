a9c4114755de8ee991c277623befb17371475989
Advait Junnarkar <advaitju@users.noreply.github.com>
2017-03-10 17:48:28 +1100

    Removed fade effect for Projects gallery tiles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
 $( document ).ready(function() {
 
-	$('.block__content__gallery-image').on('mouseenter', function() {
-		$(this).find('.block__content__gallery-image__description').fadeIn(250)
-	});
-	$('.block__content__gallery-image__description').on('mouseleave', function() {
-		$(this).fadeOut(250)
-	});
-
 });
